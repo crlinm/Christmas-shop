0af634a85534c99474917523529751ce205b5f86
Alina M <crlin.m@gmail.com>
2024-11-27 00:55:57 +0500

    feat: main gifts - random gift cards from json

---
+++ christmas-shop/src/js/index.js
@@ -0,0 +1,21 @@
+import {Gift} from './Gift.js';
+import {getGifts, randomIndex} from './utils.js';
+
+GIFTS_GRID_HOME = document.querySelector('.gift_cards__columns');
+const cntCards = 4;
+
+
+async function init() {
+    const data = await getGifts();
+    const gifts = data.map(e => new Gift(e));
+    let n = gifts.length;
+    for (let i = 0; i < cntCards; i += 1) {
+        const iRandom = Math.floor(Math.random() * (n - 1));
+        const gift = gifts[iRandom];
+        gifts[iRandom] = gifts[n - 1];
+        n -= 1;
+        GIFTS_GRID_HOME.append(gift.createGift());
+    }
+}
+
+init();
