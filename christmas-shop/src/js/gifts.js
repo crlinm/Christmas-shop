d9337ffea14af19e67c4f58104702a6d4e246274
Alina M <crlin.m@gmail.com>
2024-11-28 02:59:33 +0500

    feat: gifts page - show only chosen gifts

--- christmas-shop/src/js/gifts.js
+++ christmas-shop/src/js/gifts.js
@@ -6,30 +6,19 @@ const GIFTS_GRID = document.querySelector(".layout-grid");
 const GIFTS_TAGS = document.querySelector('.gifts__tags');
 
 
-async function init() {
-    const gifts = await getGifts();
-    const cntGifts = gifts.length;
-
-    // const gift = document.createElement('div');
-    // gift.classList.add("layout-grid");
-
-    for (let i = 0; i < cntGifts; i += 1) {
-        const gift = new Gift(gifts[i]);
-        GIFTS_GRID.append(gift.createGift());
-    }
-}
-
-init();
-
-GIFTS_TAGS.addEventListener("click", (e) => {
+async function clickTag(e) {
     console.log(e, e.target.innerText);
     if (e.target.classList.contains('tag__category')) {
         const currentTag = e.target;
         console.log(currentTag);
         clearChosenTags();
         currentTag.classList.add('gift__tag__chosen');
+
+        giftChosen = await showGiftsByTag(currentTag.innerText);
+        console.log(giftChosen);
+        showGifts(giftChosen);
     }
-});
+}
 
 const clearChosenTags = () => {
     const tags = document.querySelectorAll(".tag__category");
@@ -39,3 +28,47 @@ const clearChosenTags = () => {
         tag.classList.add('gift__tag__not_chosen');
     });
 }
+
+async function showGiftsByTag(value) {
+    const gifts = await getGifts();
+    const cntGifts = gifts.length;
+
+    if (value === 'ALL') {
+        return gifts;
+    }
+
+    const res = [];
+    for (let i = 0; i < cntGifts; i += 1) {
+        const gift = new Gift(gifts[i]);
+        if (gift.category.toUpperCase() === value.toUpperCase()) {
+            res.push(gift);
+        }
+    }
+    console.log(res);
+    return res;
+}
+
+const showGifts = (gifts) => {
+    GIFTS_GRID.textContent = '';
+
+    for (let i = 0; i < gifts.length; i += 1) {
+        const gift = new Gift(gifts[i]);
+        GIFTS_GRID.append(gift.createGift());
+    }
+}
+
+
+async function init() {
+    const gifts = await getGifts();
+    const cntGifts = gifts.length;
+
+    // const gift = document.createElement('div');
+    // gift.classList.add("layout-grid");
+
+    showGifts(gifts);
+}
+
+init();
+
+GIFTS_TAGS.addEventListener("click", (e) => clickTag(e));
+
